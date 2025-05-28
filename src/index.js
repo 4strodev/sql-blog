81041ec1c3f201722a8aa6269a46964348f20fe1
4strodev <jumarurex@gmail.com>
2025-05-28 21:50:44 +0200

    chore: format code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const morgan = require("morgan");
-const {}=require('./db/getpool')
+const { getPool } = require("./db/getpool");
 const app = express();
 
 app.use(express.json());
 app.use(morgan());
 
 app.get("/", (_, res) => {
-    return res.json({msg: "Hello world"});
-})
+  return res.json({ msg: "Hello world" });
+});
 
 app.listen(3000, () => {
-    console.log("listening on port 3000");
-})
+  console.log("listening on port 3000");
+});
